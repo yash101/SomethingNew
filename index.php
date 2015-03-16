d05b985bd551db30faa1448ff64d6cb14cd2cc91
yash101 <dlodha@pvlearners.net>
2015-03-15 19:32:18 -0700

    Save

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
 <?php
 ?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Let's Learn Something New!</title>
+		<link rel="stylesheet" type="text/css" href="Stylesheets/theme.css">
+	</head>
+</html>
