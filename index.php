590b0b17f4f41245e2cd875de835a5d57bce7a1e
yash101 <dlodha@pvlearners.net>
2015-03-15 21:45:06 -0700

    Save

--- index.php
+++ index.php
@@ -6,7 +6,15 @@
 	<head>
 		<title>Let's Learn Something New!</title>
 		<link rel="stylesheet" type="text/css" href="Stylesheets/theme.css">
+		<link rel="stylesheet" type="text/css" href="Stylesheets/page_layout.css">
 	</head>
 	<body>
+		<div id="page">
+		</div>
+		<div id="navigation">
+		</div>
+		<div id="topnav">
+			<div id="login_btn">Log In!</div>
+		</div>
 	</body>
 </html>
